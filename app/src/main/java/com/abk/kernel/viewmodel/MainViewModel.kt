92b91606ae3faf014e58d9d6df86e92b9ce36db6
xingguangcuican6666 <xingguangcuican666@foxmail.com>
2026-05-07 07:59:21 +0000

    Fix missing Uri import

--- app/src/main/java/com/abk/kernel/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/abk/kernel/viewmodel/MainViewModel.kt
@@ -5,6 +5,7 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.net.Uri
 import android.os.Build
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
